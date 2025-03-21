26883382d3808378d7fec40a32ab8e7146b91fc5
OllieK9 <167331049+OllieK9@users.noreply.github.com>
2025-03-21 14:39:41 +0000

    yessir

--- Program.cs
+++ Program.cs
@@ -3,6 +3,6 @@ using Microsoft.AspNetCore.Builder;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello from Azure! This is a simple .NET website.");
+app.MapGet("/", () => "Hello from Azure! This is a simple .NET website. YESSIRSKI");
 
 app.Run();
